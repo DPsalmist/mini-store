d119be6ea7fe327934917f3fe84032dc60dd2d25
Samuel Damilare <48826043+DPsalmist@users.noreply.github.com>
2020-06-06 12:37:17 +0100

    Initial Comit

---
+++ buy an item.py
@@ -0,0 +1,128 @@
+print ("Welcome to SOD Mini Store")
+print ('Make sure you start every item with a capital letter!')
+
+name = input("Kindly enter yur name: ")
+
+print ("Hello ", name, "I have here: 1.Garri, 2.Yam, 3.Rice, 4.Beans, 5.Spaghetti and 6.Semovita",)
+
+#Numitem = eval(input ("How many items do you want to buy? "))
+
+Garri = 500
+Yam = 350
+Rice = 230
+Beans = 400
+Spaghetti = 200
+Semovita = 600
+
+#String = eval(input())
+
+#for x <= Numitem:
+item1 = eval(input("Enter item 1: "))
+if item1 == Garri:
+    print ("The cost of item 1 is: #",Garri)
+elif item1 == Yam:
+    print ("The cost of item 1 is: #",Yam)
+elif item1 == Rice:
+    print ("The cost of item 1 is: #",Rice)
+elif item1 == Beans:
+    print ("The cost of item 1 is: #",Beans)
+elif item1 == Spaghetti:
+    print ("The cost of item 1 is: #",Spaghetti)
+elif item1 == Semovita:
+    print ("The cost of item 1 is:#",Semovita)
+elif item1 == String:
+    print ("Your item is not on my list!")
+else:
+    print ("Done !")
+               
+item2 = eval(input("Enter item 2: "))
+if item2 == Garri:
+    print ("The cost of item 2 is: #",Garri)
+elif item2 == Yam:
+    print ("The cost of item 2 is: #",Yam)
+elif item2 == Rice:
+    print ("The cost of item 2 is: #",Rice)
+elif item2 == Beans:
+    print ("The cost of item 2 is: #",Beans)
+elif item2 == Spaghetti:
+    print ("The cost of item 2 is: #",Spaghetti)
+elif item2 == Semovita:
+    print ("The cost of item 2 is:#",Semovita)
+else:
+    print ("Your item is not on my list!")
+
+
+item3 = eval(input ("Enter item 3: "))
+if item3 == Garri:
+    print ("The cost of item 3 is: #",Garri)
+elif item3 == Yam:
+    print ("The cost of item 3 is: #",Yam)
+elif item3 == Rice:
+    print ("The cost of item 3 is: #",Rice)
+elif item3 == Beans:
+    print ("The cost of item 3 is: #",Beans)
+elif item3 == Spaghetti:
+    print ("The cost of item 3 is: #",Spaghetti)
+elif item3 == Semovita:
+    print ("The cost of item 3 is:#",Semovita)
+else:
+    print ("Your item is not on my list!")
+
+
+item4 = eval(input ("Enter item 4: "))
+if item4 == Garri:
+    print ("The cost of item 4 is: #",Garri)
+elif item4 == Yam:
+    print ("The cost of item 4 is: #",Yam)
+elif item4 == Rice:
+    print ("The cost of item 4 is: #",Rice)
+elif item4 == Beans:
+    print ("The cost of item 4 is: #",Beans)
+elif item4 == Spaghetti:
+    print ("The cost of item 4 is: #",Spaghetti)
+elif item4 == Semovita:
+    print ("The cost of item 4 is:#",Semovita)
+else:
+    print ("Your item is not on my list!")
+
+item5 = eval(input ("Enter item 5: "))
+if item5 == Garri:
+    print ("The cost of item 5 is: #",Garri)
+elif item5 == Yam:
+    print ("The cost of item 5 is: #",Yam)
+elif item5 == Rice:
+    print ("The cost of item 5 is: #",Rice)
+elif item5 == Beans:
+    print ("The cost of item 5 is: #",Beans)
+elif item5 == Spaghetti:
+    print ("The cost of item 5 is: #",Spaghetti)
+elif item5 == Semovita:
+    print ("The cost of item 5 is:#",Semovita)
+else:
+    print ("Your item is not on my list!")
+
+
+item6 = eval(input ("Enter item 6: "))
+if item6 == Garri:
+    print ("The cost of item 6 is: #",Garri)
+elif item6 == Yam:
+    print ("The cost of item 6 is: #",Yam)
+elif item6 == Rice:
+    print ("The cost of item 6 is: #",Rice)
+elif item6 == Beans:
+    print ("The cost of item 6 is: #",Beans)
+elif item6 == Spaghetti:
+    print ("The cost of item 6 is: #",Spaghetti)
+elif item6 == Semovita:
+    print ("The cost of item 6 is:#",Semovita)
+else:
+    print ("Your item is not on my list!", end = "")
+
+
+
+total = item1 + item2 + item3 + item4 + item5 + item6
+
+print ("Your total cost is: #",total)
+print ("Thank you ", name, "for using SOD Mini Store.")
+
+
